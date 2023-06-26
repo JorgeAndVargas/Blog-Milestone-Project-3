2d11a988d7172bcd744f5770c8f6b70fd75cc756
Jorge Vargas <jorgevargas@jorges-mbp.mynetworksettings.com>
2023-06-26 18:15:28 -0400

    edit post funtionality and routes added

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -140,6 +140,51 @@ router.post('/add-post', authMiddleware, async (req, res) => {
   }
 });
 
+
+//GET
+//EDIT POST
+router.get('/edit-post/:id', authMiddleware, async (req, res) => {
+  try {
+
+    const locals = {
+      title: "Edit Post",
+      description: "Free NodeJs User Management System",
+    };
+
+    const data = await Post.findOne({ _id: req.params.id });
+
+    res.render('admin/edit-post', {
+      locals,
+      data,
+      layout: adminLayout
+    })
+
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+//PUT
+//EDIT POST
+router.put('/edit-post/:id', authMiddleware, async (req, res) => {
+  try {
+
+    await Post.findByIdAndUpdate(req.params.id, {
+      title: req.body.title,
+      image: req.body.image,
+      body: req.body.body,
+      updatedAt: Date.now()
+    });
+
+    res.redirect(`/edit-post/${req.params.id}`);
+
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
 //POST
 //ADMIN REGISTER
 
