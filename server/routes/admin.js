9f2094d957bf7506c9094a4e64e8a4044f02c9ef
Jorge Vargas <jorgevargas@jorges-mbp.mynetworksettings.com>
2023-06-26 17:30:37 -0400

    added tokens, hashed password for register, added sessions mongostore

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/post');
 const User = require('../models/user');
+const becrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const adminLayout = '../views/layouts/admin'
 
@@ -35,6 +37,30 @@ router.post('/admin', async (req, res) => {
       res.send('You are logged in.')
     }else {
       res.send ('Wrong username or password');
+    }
+
+  } catch (error) { 
+    console.log(error);
+  }
+});
+
+//POST
+//ADMIN REGISTER
+
+router.post('/register', async (req, res) => {
+  try {
+
+    const { username, password } = req.body;
+    const hasedPassword = await becrypt.hash(password, 10);
+
+    try {
+      const user = await User.create({username, password:hasedPassword});
+      res.status(201).jason({ message: 'User Created', user});
+    } catch (error) {
+      if (error.cose === 11000) {
+        res.status(409).json({ message: 'user already in use'});
+      }
+      res.status(500).json({ message: 'Internal server error'});
     }
 
   } catch (error) { 
